4c7f54e536c87123c36aedf977615a384d68a9a4
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-05 00:56:37 -0800

    Create FinalKeyword.java

---
+++ FinalKeyword.java
@@ -0,0 +1,12 @@
+public class FinalDemo
+{
+	final float PI;
+	{                         //INSTANCE BLOCK
+		PI = 3.1425f;
+	}
+	public static void main(String args[]args)
+	{
+	}
+}
+
+//FINAL CLASS CANNOT BE INHERITED		
