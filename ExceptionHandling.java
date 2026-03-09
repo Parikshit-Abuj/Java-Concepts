e69683bb6d5f0f057601e82f957bbe640116f425
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-09 09:18:46 +0530

    Update ExceptionHandling.java

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -209,3 +209,79 @@ public class TryWithResourcesExample {
 
     }
 }
+
+//USER DEFINED EXCEPTION
+package scexception1;
+
+class StackOverFlow extends Exception {
+    public String toString() {
+        return "Stack is Full";
+    }
+}
+
+class StackUnderFlow extends Exception {
+    public String toString() {
+        return "Stack is Empty";
+    }
+}
+
+class Stack {
+    private int size;
+    private int top = -1;
+    private int S[];
+
+    public Stack(int sz) {
+        size = sz;
+        S = new int[sz];
+    }
+
+    public void push(int x) throws StackOverFlow {
+        if (top == size - 1)
+            throw new StackOverFlow();
+
+        top++;
+        S[top] = x;
+    }
+
+    public int pop() throws StackUnderFlow {
+        int x;
+
+        if (top == -1)
+            throw new StackUnderFlow();
+
+        x = S[top];
+        top--;
+        return x;
+    }
+}
+
+public class SCException1 {
+    public static void main(String[] args) {
+
+        Stack st = new Stack(5);
+
+        try {
+            st.push(10);
+            st.push(15);
+            st.push(20);
+            st.push(25);
+            st.push(30);
+            st.push(35); // This will cause StackOverFlow
+        } 
+        catch (StackOverFlow s) {
+            System.out.println(s);
+        }
+
+        try {
+            System.out.println("Popped: " + st.pop());
+            System.out.println("Popped: " + st.pop());
+            System.out.println("Popped: " + st.pop());
+            System.out.println("Popped: " + st.pop());
+            System.out.println("Popped: " + st.pop());
+            System.out.println("Popped: " + st.pop()); // This will cause StackUnderFlow
+        } 
+        catch (StackUnderFlow s) {
+            System.out.println(s);
+        }
+    }
+}
