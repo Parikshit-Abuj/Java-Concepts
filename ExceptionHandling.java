9c5b9b6b98a2b2eb0f4b57ffb7d58e9a7f7133f6
Parikshit-Abuj <parikshitabuj94@gmail.com>
2026-03-09 08:57:51 +0530

    Update ExceptionHandling.java
    Added TryWithResourcesExample class demonstrating try-with-resources for file reading.

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -191,3 +191,21 @@ class Test
         }
     }
 }
+
+//TRY WITH RESOURCE 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResourcesExample {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
+            String line = br.readLine();
+            System.out.println(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
